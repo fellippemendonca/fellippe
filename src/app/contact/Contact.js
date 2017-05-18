b48e9af3bab473c35b777cf895535b99a41b2d53
fellippemendonca <fellippe.mendonca@gmail.com>
2017-05-18 02:18:32 -0300

    css again

--- src/app/contact/Contact.js
+++ src/app/contact/Contact.js
@@ -12,13 +12,13 @@ function Contact(props) {
         <h1>Contacts</h1>
         <div>
             <a href="skype:fellippe.mendonca?chat">
-              <img src={skype} className="Skype" alt="skype" style={{width: 100, height: 100}}/>
+              <img src={skype} className="Skype" alt="skype" style={{width: 100, height: 100, padding: '2%'}}/>
             </a>
             <a href="mailto::fellippe.mendonca@gmail.com">
-              <img src={gmail} className="Gmail" alt="gmail" style={{width: 100, height: 100}}/>
+              <img src={gmail} className="Gmail" alt="gmail" style={{width: 100, height: 100, padding: '2%'}}/>
             </a>
             <a href="https://www.linkedin.com/in/fellippe-de-lelis-mendon%C3%A7a-799446a0/">
-              <img src={linkedin} className="Linkedin" alt="linkedin" style={{width: 100, height: 100}}/>
+              <img src={linkedin} className="Linkedin" alt="linkedin" style={{width: 100, height: 100, padding: '2%'}}/>
             </a>
         </div>
       </div>
