55030009ee58036747c9508c6a645a5ff093ac29
fellippemendonca <fellippe.mendonca@gmail.com>
2017-05-18 01:58:31 -0300

    css again

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
+import image1 from './assets/images/valley-1347.jpg';
+import image2 from './assets/images/sao-paulo-1346.jpg';
+import image3 from './assets/images/empty-office-1346.jpg';
+import image4 from './assets/images/telegraph-1346.jpg';
 import Menu from './app/menu/Menu';
-import './assets/images/valley-1347.jpg';
-import './assets/images/sao-paulo-1346.jpg';
-import './assets/images/empty-office-1346.jpg';
-import './assets/images/telegraph-1346.jpg';
-
 
 ReactDOM.render(
   <Menu/>,
