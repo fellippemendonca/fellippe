8b285b8b850114d7b5dceb842b96676207f529c0
fellippemendonca <fellippe.mendonca@gmail.com>
2017-05-25 02:11:52 -0300

    ReactLink

--- src/app/home/Home.js
+++ src/app/home/Home.js
@@ -9,7 +9,9 @@ function Home(props) {
     <div className="Contents">
       <div className="Home">
         <h1>Hi, I'm {props.owner}!</h1>
-        <img src={logo} className="Home-logo" alt="logo" />
+        <a href="https://facebook.github.io/react/">
+          <img src={logo} className="Home-logo" alt="logo"/>
+        </a>
         <h2>Software Developer at</h2>
           <a href="http://www.viavarejo.com.br/" style={linkStyle}>
             <img src={vvPng} className="vvPng" alt="vvPng" style={{width: 190, height: 130, backgroundColor:'rgba(255,255,255,0.7)'}} />
