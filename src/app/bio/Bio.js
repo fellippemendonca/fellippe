1bcdb88c4162e48575d2938f298474146c4870bf
Fellippe Mendonça <fellippe.mendonca@gmail.com>
2017-05-30 08:03:34 -0300

    hist changes

--- src/app/bio/Bio.js
+++ src/app/bio/Bio.js
@@ -16,7 +16,7 @@ function Bio(props) {
           he met a wonderful woman Izabel Fernandes, also known as my grandmother, and they didn't take too long to get married.<br/>
           Herminio and Izabel were living yet in the farm with the rest of the family while they had 4 kids all men, José, Antonio, <br/>
           Herminio, Carlo and some time later they moved to São Paulo/SP in search for a better and modern life in the great city.<br/>
-          They had 3 more kids this time all women, Izabel, Maria, Camila(my mother) while their sons not even teenagers yet where already working at a ceramic and glass factory.<br/>
+          They had 3 more kids, this time all women, Maria Izabel, Maria Aparecida, Camila (my mother) while their sons not even teenagers yet where already working at a ceramic and glass factory.<br/>
           <br/>Camila de Lelis Fellippe is the youngest of the family and her name was inspired from a promise that my grandmother did to the Saint Camillus de Lellis.<br/>
           Continue...
         </div>
@@ -25,14 +25,3 @@ function Bio(props) {
   );
 }
 export default Bio;
-
-/*
-          <br/>As a child used to play outdoor riding bicycle, on rainy days LEGO instead (because build things is so awesome). Videogames wasn't THAT fun yet.<br/>
-          <br/>As teenager used to play on PC or Nintendo64 with friends, it was soo cool... Some time after the taste for Rock and Metal<br/> 
-          made us all willing to play instruments and start a band, and there my passion for <b>Music</b> began.<br/>
-          <br/>College started so the new jobs. The band is dismissed cuz no one have that much "free" time with all <br/>
-          new adult responsibilities. Yet alone and without free time I kept playing, recording, buying new instruments (until today).<br/>
-          <br/>Adult life, income surplus and a new passion discovered, <b>Motorcycles</b>. Yeah, Rock'n'ride! \m/<br/>
-          <br/>Willing to discover the world by living in a distinct place I acquired <b>Italian citizenship</b> by descent.<br/>
-          <br/>To Do...
-*/
