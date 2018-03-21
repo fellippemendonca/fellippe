3b200374042c45d7fd81bddbcc6482b5b81f8db3
Fellippe Mendonça <fellippe.mendonca@gmail.com>
2018-03-21 14:35:05 -0300

    Update work.js

--- src/app/work/Work.js
+++ src/app/work/Work.js
@@ -6,16 +6,15 @@ function Work(props) {
     <div className="Contents">
       <div className="Work">
         <h1>Working History</h1>
-        <div>Accomplished IT executive with over 9 years of experience in E-Commerce market, <br/>
-        acting from QA, System Analyst, System Developer and now team leader as Scrum Master <br/>
-        with knowledge in various fronts of current technologies such as JavaScript, Node.js, Mongo DB, Oracle DB, and Kafka. <br/> 
-        Passionate about challenges and with proven records on delivering solutions, <br/>  
-        navigates the team into transforming business environments and implementing new technologies. <br/>
-        Exceptionally organized and detail-oriented enables the improvement and effectiveness of <br/>
-        the organization, as well as the increase in productivity.  <br/>
-        <br/> 
-        "There's always a smarter efficient way to achieve a goal.  <br/> 
-        The increase of solution size and complexity shows <br/> that we probably went through the wrong path."</div>
+        <div>
+    
+        Accomplished Software Developer with experience in e-commerce Marketplace platform architecture<br/>
+        and high-performance mobile apps backend services.<br/>
+        With 8 years experience in software development environment, with knowledge on various fronts<br/>
+        of current technologies such as Node.js, Go, MongoDB, Elasticsearch. Passionate about extreme challenging problems<br/>
+        and with proven records of always delivering solutions with the best performance.<br/>
+      
+        </div>
       </div>
     </div>
   );
